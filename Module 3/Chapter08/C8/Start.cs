58615ae1e9b24be26bf345d538d878c8dc02659a
TechVish <techvish@github.com>
2017-06-14 18:36:36 +0530

    Code Files Added

---
+++ Module 3/Chapter08/C8/Start.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C8
+{
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            //Program1.Product(args);
+            //Program1.Power(args);
+            Program1.AdaptivePower(args);
+            Console.ReadLine();
+        }
+    }
+}
